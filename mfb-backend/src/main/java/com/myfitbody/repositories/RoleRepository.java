da72c5392d2834f2971e7d42b681d3c67b523b7d
Bruno Lessa <Brunolessa@live.com>
2024-01-03 23:24:39 -0300

    feat: create role repository

---
+++ mfb-backend/src/main/java/com/myfitbody/repositories/RoleRepository.java
@@ -0,0 +1,14 @@
+package com.myfitbody.repositories;
+
+import com.myfitbody.domain.role.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, UUID> {
+
+    Optional<Role> findByName(String name);
+}
