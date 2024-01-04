120ff5f3572eebdb3a98ea7827154cba1f340697
Bruno Lessa <brunolessa@live.com>
2024-01-03 22:26:26 -0300

    feat: create backend project

---
+++ mfb-backend/src/main/java/com/myfitbody/MfbBackendApplication.java
@@ -0,0 +1,13 @@
+package com.myfitbody;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MfbBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MfbBackendApplication.class, args);
+    }
+
+}
---
+++ mfb-backend/src/test/java/com/myfitbody/MfbBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myfitbody;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MfbBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
