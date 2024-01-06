eae9c58cfa88137470353ebb2e2c368e5748d325
Bruno Lessa <Brunolessa@live.com>
2024-01-06 19:46:51 -0300

    feat: user service impl

---
+++ mfb-backend/src/main/java/com/myfitbody/services/impl/UserServiceImpl.java
@@ -0,0 +1,198 @@
+package com.myfitbody.services.impl;
+
+import com.myfitbody.domain.exceptions.DatabaseException;
+import com.myfitbody.domain.exceptions.ResourceNotFoundException;
+import com.myfitbody.domain.role.Role;
+import com.myfitbody.domain.role.RoleRequestDTO;
+import com.myfitbody.domain.user.*;
+import com.myfitbody.repositories.RoleRepository;
+import com.myfitbody.repositories.UserRepository;
+import com.myfitbody.services.contracts.EmailService;
+import com.myfitbody.services.contracts.UserService;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserDetailsService, UserService {
+
+    private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
+    private final EmailService emailService;
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws ResourceNotFoundException {
+            return userRepository
+                    .findByEmailIgnoreCase(username)
+                    .orElseThrow(() -> new ResourceNotFoundException("User not found"));
+    }
+
+    @Transactional(readOnly = true)
+    @Override
+    public Page<UserResponseDTO> getAllUsers(Pageable pageable, String search) {
+        return userRepository
+                .findAllByFirstNameIgnoreCaseStartingWithOrLastNameIgnoreCaseStartingWith(pageable, search, search)
+                .map(User::toResponseDTO);
+    }
+
+    @Transactional(readOnly = true)
+    @Override
+    public UserResponseDTO getUserById(UUID id) {
+        return userRepository
+                .findById(id)
+                .map(User::toResponseDTO)
+                .orElseThrow(() -> new ResourceNotFoundException("User not found id: " + id));
+    }
+
+    @Transactional
+    @Override
+    public UserResponseDTO createUser(UserCreateDTO dto) {
+        UUID tokenVerifyEmail = UUID.randomUUID();
+        String password = passwordEncoder.encode(dto.password());
+        Role role = roleRepository
+                .findByName("ROLE_USER")
+                .orElseThrow(() -> new ResourceNotFoundException("Role not found"));
+
+        List<Role> userRoles = dto.roles() == null
+                ? List.of(role)
+                : roleRepository
+                .findRolesByNameIn(
+                        dto.roles()
+                                .stream()
+                                .map(RoleRequestDTO::name)
+                                .toList()
+                );
+
+        User user = User.builder()
+                .firstName(dto.firstName())
+                .lastName(dto.lastName())
+                .email(dto.email())
+                .password(password)
+                .tokenVerifyEmail(tokenVerifyEmail)
+                .isActive(false)
+                .inactiveMessage("User email not verified")
+                .roles(userRoles)
+                .build();
+
+        var entity = userRepository.save(user);
+
+        String body = """
+                <p>Clique abaixo para validar seu e-mail:</p>
+                <a href="%s"
+                    style="
+                        font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif;
+                        box-sizing: border-box;
+                        font-size: 14px;
+                        color: #FFF;
+                        text-decoration: none;
+                        line-height: 2em;
+                        font-weight: bold;
+                        text-align: center;
+                        cursor: pointer;
+                        display: inline-block;
+                        border-radius: 5px;
+                        text-transform: capitalize;
+                        background-color: #f5a967;
+                        margin: 0;
+                        padding: 3px 6px;"
+                >
+                	Clique aqui
+                </a>
+                <p>Se você não realizou o cadastro em nosso site, por favor ignore este e-mail.</p>
+                """.formatted("http://" + "localhost:8080" + "/api/v1/users/verify-email/" + tokenVerifyEmail);
+
+        emailService.sendHtmlEmail(
+                entity.getEmail(),
+                "Verifique seu e-mail",
+                body
+        );
+
+        return entity.toResponseDTO();
+    }
+
+    @Transactional
+    @Override
+    public UserResponseDTO updateUser(UUID id, UserEditDTO dto) {
+        User user = userRepository
+                .findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("User not found id: " + id));
+
+        user.setFirstName(dto.firstName());
+        user.setLastName(dto.lastName());
+
+        return userRepository
+                .save(user)
+                .toResponseDTO();
+    }
+
+    @Transactional
+    @Override
+    public boolean updateUserPassword(UUID id, UserEditPasswordDTO dto) {
+        User user = userRepository
+                .findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("User not found id: " + id));
+
+        if (!passwordEncoder.matches(dto.oldPassword(), user.getPassword())) {
+            return false;
+        }
+
+        user.setPassword(passwordEncoder.encode(dto.newPassword()));
+
+        userRepository.save(user);
+
+        return true;
+    }
+
+    @Transactional
+    public UserResponseDTO updateUserEmail(UUID id, UserEditEmailDTO dto) {
+        User user = userRepository
+                .findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("User not found id: " + id));
+
+        user.setEmail(dto.email());
+
+        return userRepository
+                .save(user)
+                .toResponseDTO();
+    }
+
+    @Transactional
+    @Override
+    public boolean verifyEmail(UUID token) {
+        User user = userRepository
+                .findByTokenVerifyEmail(token)
+                .orElseThrow(() -> new ResourceNotFoundException("User not found token: " + token));
+
+        user.setTokenVerifyEmail(null);
+        user.setInactiveMessage(null);
+        user.setActive(true);
+
+        userRepository.save(user);
+
+        return true;
+    }
+
+    @Transactional
+    @Override
+    public void deleteUser(UUID id) {
+        try {
+            roleRepository.deleteById(id);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException("ID not found:" + id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException("Integrity violation");
+        }
+    }
+}
