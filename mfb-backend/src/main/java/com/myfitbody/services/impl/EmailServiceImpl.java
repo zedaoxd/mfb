3413b9d2dd6a9e701e06dd1b6f397ef473c16b8f
Bruno Lessa <Brunolessa@live.com>
2024-01-06 19:46:43 -0300

    feat: email service impl

---
+++ mfb-backend/src/main/java/com/myfitbody/services/impl/EmailServiceImpl.java
@@ -0,0 +1,56 @@
+package com.myfitbody.services.impl;
+
+import com.myfitbody.services.contracts.EmailService;
+import jakarta.mail.internet.MimeMessage;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+@Service
+@RequiredArgsConstructor
+public class EmailServiceImpl implements EmailService {
+
+    private static final String UTF_8_ENCODING = "UTF-8";
+    private static final String EMAIL_TEMPLATE = "email-template";
+    @Value("${spring.mail.verify.host}")
+    private String host;
+    @Value("${spring.mail.username}")
+    private String fromEmail;
+    private final JavaMailSender emailSender;
+    private final TemplateEngine templateEngine;
+
+    @Override
+    @Async
+    public void sendHtmlEmail(String to, String subject, String body) {
+        try {
+            Context context = new Context();
+            context.setVariable("body", body);
+            String text = templateEngine.process(EMAIL_TEMPLATE, context);
+            MimeMessage message = getMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message, true, UTF_8_ENCODING);
+            helper.setPriority(1);
+            helper.setFrom(fromEmail);
+            helper.setTo(to);
+            helper.setSubject(subject);
+            helper.setText(text, true);
+            emailSender.send(message);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+
+    @Override
+    @Async
+    public void sendHtmlEmailWithAttachments(String to, String subject, String body, String... attachmentsPath) {
+    }
+
+    private MimeMessage getMimeMessage() {
+        return emailSender.createMimeMessage();
+    }
+}
