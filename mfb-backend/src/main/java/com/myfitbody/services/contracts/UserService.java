398b5002df932e1f49b388e6326a1c764b37a5a6
Bruno Lessa <Brunolessa@live.com>
2024-01-06 19:46:24 -0300

    feat: user service contract

---
+++ mfb-backend/src/main/java/com/myfitbody/services/contracts/UserService.java
@@ -0,0 +1,19 @@
+package com.myfitbody.services.contracts;
+
+import com.myfitbody.domain.user.*;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.UUID;
+
+public interface UserService {
+
+    Page<UserResponseDTO> getAllUsers(Pageable pageable, String search);
+    UserResponseDTO getUserById(UUID id);
+    UserResponseDTO createUser(UserCreateDTO dto);
+    UserResponseDTO updateUser(UUID id, UserEditDTO dto);
+    boolean updateUserPassword(UUID id, UserEditPasswordDTO dto);
+    UserResponseDTO updateUserEmail(UUID id, UserEditEmailDTO dto);
+    boolean verifyEmail(UUID token);
+    void deleteUser(UUID id);
+}
