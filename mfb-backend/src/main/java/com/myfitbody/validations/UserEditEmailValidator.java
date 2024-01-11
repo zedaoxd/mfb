de9b90d92bb6e5fd1268318d05c474ffe6122bc4
Bruno Lessa <Brunolessa@live.com>
2024-01-10 21:52:47 -0300

    refactor: improves quality

--- mfb-backend/src/main/java/com/myfitbody/validations/UserEditEmailValidator.java
+++ mfb-backend/src/main/java/com/myfitbody/validations/UserEditEmailValidator.java
@@ -6,38 +6,28 @@ import com.myfitbody.domain.exceptions.ResourceNotFoundException;
 import com.myfitbody.domain.user.User;
 import com.myfitbody.domain.user.UserEditEmailDTO;
 import com.myfitbody.repositories.UserRepository;
-import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
-import org.springframework.web.servlet.HandlerMapping;
 import lombok.RequiredArgsConstructor;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-import java.util.UUID;
 
 @RequiredArgsConstructor
 public class UserEditEmailValidator implements ConstraintValidator<UserEditEmailValid, UserEditEmailDTO> {
 
-    private final HttpServletRequest request;
     private final UserRepository repository;
 
     @Override
     public boolean isValid(UserEditEmailDTO dto, ConstraintValidatorContext context) {
-
-        var uriVars = request.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
-        UUID id = UUID.fromString((String) ((Map<?, ?>) uriVars).get("id"));
-
         List<FieldMessage> list = new ArrayList<>();
 
         User entity = repository
-                .findByEmailIgnoreCase(dto.email())
-                .orElseThrow(() -> new ResourceNotFoundException("User not found, email: " + dto.email()));
+                .findByEmailIgnoreCase(dto.oldEmail())
+                .orElseThrow(() -> new ResourceNotFoundException("User not found, email: " + dto.oldEmail()));
 
-        if (entity != null && !entity.getId().equals(id)) {
-            list.add(new FieldMessage("email", "This email is already in use by another user"));
-        }
+        verifyIfEqualEmails(entity.getEmail(), dto.newEmail(), list);
+        verifyIfEmailExists(dto.newEmail(), list);
 
         for (FieldMessage e : list) {
             context.disableDefaultConstraintViolation();
@@ -45,5 +35,17 @@ public class UserEditEmailValidator implements ConstraintValidator<UserEditEmail
                     .addConstraintViolation();
         }
         return list.isEmpty();
+    }
+
+    private void verifyIfEqualEmails(String oldEmail, String newEmail, List<FieldMessage> list) {
+        if (oldEmail.equalsIgnoreCase(newEmail)) {
+            list.add(new FieldMessage("newEmail", "The new email cannot be the same as the old email"));
+        }
+    }
+
+    private void verifyIfEmailExists(String newEmail, List<FieldMessage> list) {
+        if (repository.existsUserByEmailIgnoreCase(newEmail)) {
+            list.add(new FieldMessage("newEmail", "This email is already in use"));
+        }
     }
 }
